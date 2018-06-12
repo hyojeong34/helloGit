1cce444756f766cb97046b98e141d5ff30d17f49
hyojeong34 <hoyjung34@naver.com>
2018-06-12 13:25:55 +0900

    test1브랜치 시작!!

--- HelloGit/src/com/kh/git/Test2.java
+++ HelloGit/src/com/kh/git/Test2.java
@@ -5,6 +5,8 @@ public class Test2 {
 	public static void main(String[] args) {
 		System.out.println("안녕, 깃으로 본 세상");
 		System.out.println("버전추가!!!!");
+		
+		System.out.println("test1브랜치 시작!!");
 
 	}
 
